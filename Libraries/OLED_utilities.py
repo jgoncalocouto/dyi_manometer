f5b53d79da063a39cc146d77ad639a77ef987ae5
jognc <jgoncalocouto@gmail.com>
2023-04-05 23:21:24 +0100

    Added condition to. check if "Type" exists as attribute check if message is to long check if Line is above the maximum number

--- Libraries/OLED_utilities.py
+++ Libraries/OLED_utilities.py
@@ -4,10 +4,21 @@ def displayMsg(oled, DisplayMode,Msg):
     if oled.width==128 and oled.height==64:
         NoLineMax=8
         StrLengthMax=16
+        
+    if "Type" not in DisplayMode:
+        raise ValueError("DisplayMode dictionary must contain a 'Type' key")
     
     # Display-mode Settings:
     if DisplayMode["Type"]=="DisplaybyLine":
-        oled.text(Msg,0,mode_parameters["LineNo"]+1,1)
+        
+        if len(Msg)>StrLengthMax:
+            Msg=Msg[0:StrLengthMax]
+            print("Warning: Message too lengthy to be displayed in the desired line. Maximum set of characters displayed, the rest was discarded.")
+
+        if DisplayMode["LineNo"]>NoLineMax-1:
+            raise ValueError("Desired line is higher than the Max Line:"+str(NoLineMax)+" | Not displayed")
+        
+        oled.text(Msg,0,DisplayMode["LineNo"]+1,1)
         oled.show()
         
         
